f11aca20dba53c524922150a702a6406fbd8090a
Jo Walsh <metazool@fastmail.net>
2015-06-20 17:56:59 +0100

    stop trying to bind to port 5000

--- app/pyserver/app.py
+++ app/pyserver/app.py
@@ -8,4 +8,8 @@ app = Flask(__name__)
 def welcome():
     return "welcome"
 
-app.run(debug=True)
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
