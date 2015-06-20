2eebb7aa54b17669c029f6d8f5039d75edef1033
Jo Walsh <metazool@fastmail.net>
2015-06-20 22:25:36 +0100

    failing API tests

--- app/pyserver/tests/test_api.py
+++ app/pyserver/tests/test_api.py
@@ -4,12 +4,22 @@ test_app = app.app.test_client()
 
 import unittest
 
+
 class ApiTest(unittest.TestCase):
+
     def test_get_recent_finds(self):
         data = test_app.get('/api/finds/recent/')
-        print data        
         parsed = json.loads(data.data)
         self.assertIsNotNone(parsed)
+        self.assertIsNotNone(parsed[0]['_id'])
+
+    def test_get_latest_quest(self):
+        data = test_app.get('/api/quest/')
+        parsed = json.loads(data.data)
+        self.assertIsNotNone(parsed)
+        self.assertIsNotNone(parsed['_id'])
+
+
 
 
 
